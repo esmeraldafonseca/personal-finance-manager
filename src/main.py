6a6c198d660f94f4d96760b2ac1dc24637dfdc21
Esmeralda Fonseca <esmeralda.fonseca2003@gmail.com>
2026-08-15 21:40:01 +0100

    feat: adiciona as novas views ao main

--- src/main.py
+++ src/main.py
@@ -8,15 +8,21 @@ import flet as ft
 
 from database.database import Database
 from repositories.transaction_repository import TransactionRepository
+from repositories.user_repository import UserRepository
 from services.reports import FinancialReport
-from ui.app_layout import AppLayout
+from services.auth_service import AuthService
+from ui.views.login_view import LoginView
+
 
 def main(page: ft.Page):
     database = Database()
-    repository = TransactionRepository(database)
-    report_service = FinancialReport(repository)
-    AppLayout(page, repository, report_service)
+    transaction_repository = TransactionRepository(database)
+    user_repository = UserRepository(database)
+    report_service = FinancialReport(transaction_repository)
+    auth_service = AuthService(user_repository)
+
+    LoginView(page, auth_service, transaction_repository, user_repository, report_service)
+
 
 if __name__ == "__main__":
     ft.run(main)
-
